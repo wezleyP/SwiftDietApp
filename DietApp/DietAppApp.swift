d511671455adf38ec5f0b7f0a513b25189e141fd
wezleyp <wesley.patterson121@gmail.com>
2022-05-07 01:30:23 -0500

    Initial Commit

---
+++ DietApp/DietAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietAppApp.swift
+//  DietApp
+//
+//  Created by Wesley Patterson on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DietAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
