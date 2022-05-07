a8352a6e087c94b810f589166684d409e2b91e16
wezleyp <wesley.patterson121@gmail.com>
2022-05-07 02:13:28 -0500

    first

--- DietApp/ContentView.swift
+++ DietApp/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var protein = ""
+    @State private var time = ""
+    // Figure out a time Picker
+    
+    var collections = [
+        Boi(name: "Kodiak", protein: 12, time: 10)
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+                
+        VStack {
+            TextField(
+                    "Enter name",
+                    text: $name
+                )
+            TextField(
+                    "Enter amount of protein",
+                    text: $protein
+                )
+            TextField(
+                    "Enter time",
+                    text: $time
+                )
+            List(collections, id: \.name) { index in
+                    
+                LabelRow(collection: index)
+                    
+            }
+            Spacer()
+        }
+            
+    }
+    
+    
+    struct Boi {
+        let name: String
+        let protein: Int
+        let time: Int
+    }
+    
+    struct LabelRow: View {
+        var collection: Boi
+        var body: some View {
+                HStack {
+                    Text("\(collection.name)")
+                    Spacer()
+                    Text("Protein: \(collection.protein) g")
+                    Text("Time: \(collection.time)")
+                }
+        }
     }
 }
 
@@ -19,3 +67,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+/*
+ 
+ protein counter
+ 
+ */
